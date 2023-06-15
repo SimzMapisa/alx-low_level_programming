a59e1deda5e530608f5f6ba8215c2e997138bc5e
SimzMapisa <simbarashe795@gmail.com>
2023-06-15 04:18:55 -0700

    fix errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'program prints a stream of string charaters'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
