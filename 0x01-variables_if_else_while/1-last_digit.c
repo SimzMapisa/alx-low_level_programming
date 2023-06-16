a1116e35a5622b4e34ffcafec320e8dcabc4f830
SimzMapisa <simbarashe795@gmail.com>
2023-06-16 06:43:05 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,6 +3,32 @@
 #include <time.h>
 /* more headers goes there */
 
+
+/**
+ * greaterOrLess - Utility function
+ *
+ * Description: 'Checks the value of lastDigit in condition'
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* lastDigit is a parameter or argument*/
+const char *greaterOrLess(int lastDigit)
+{
+	if (lastDigit > 5)
+	{
+		return ("and is greater than 5");
+	}
+	else if (lastDigit == 0)
+	{
+		return ("and is 0");
+	}
+	else
+	{
+		return ("and is less than 6 and not 0");
+	}
+}
+
 /**
  * main - Entry point
  *
@@ -19,30 +45,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	int lastDigit = n % 10;
-
-/**
- * greaterOrLess - Utility function
- *
- * Description: 'Checks the value of lastDigit in condition'
- *
- * Return: Always 0 (Success)
- */
-
-	const char *greaterOrLess(int lastDigit)
-	{
-		if (lastDigit > 5)
-		{
-			return ("and is greater than 5");
-		}
-		else if (lastDigit == 0)
-		{
-			return ("and is 0");
-		}
-		else
-		{
-			return ("and is less than 6 and not 0");
-		}
-	}
 
 	printf("Last digit of, %d, is %d %s", n, lastDigit, greaterOrLess(lastDigit));
 
