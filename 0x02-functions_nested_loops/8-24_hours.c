a3506ba73b29823aff639d541477cb09df43c65e
SimzMapisa <simbarashe795@gmail.com>
2023-06-20 04:47:54 -0700

    prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Description: "prints every minute of the day of Jack Bauer"
+ */
+
+void jack_bauer(void)
+{
+	int j, k, l, m;
+
+	for (j = 48; j <= 50; j++)
+	{
+		for (k = 48; k <= 51; k++)
+		{
+			for (l = 48; l <= 53; l++)
+			{
+				for (m = 48; m <= 57; m++)
+				{
+					_putchar(j);
+					_putchar(k);
+					_putchar(':');
+					_putchar(l);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
