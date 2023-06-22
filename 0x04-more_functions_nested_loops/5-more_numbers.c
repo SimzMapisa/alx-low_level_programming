3d09e2f538a011ee5a6ff338b8f546b7cd8d518b
SimzMapisa <simbarashe795@gmail.com>
2023-06-22 11:23:00 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,14 +17,8 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
-			{
-				_putchar('1');
-				_putchar(num % 10 + '0');
-			}
-			else
-			{
-				_putchar('0' + num);
-			}
+			_putchar('1');
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
