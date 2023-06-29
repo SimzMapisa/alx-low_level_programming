bdb2912caa1c7fc555f0f8e1cd1f138d176600b5
SimzMapisa <simbarashe795@gmail.com>
2023-06-29 07:38:12 -0700

    fix main.h

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,7 @@
  *
  * Description: "encodes a string into 1337."
  *
- * Return: a string 
+ * Return: a string
  *
  */
 
@@ -15,8 +15,8 @@ char *leet(char *strn)
 	int i = 0;
 	int j;
 	char chars[] = "aAeEoOtTlL";
-	char nums [] = "4433007711";
-	
+	char nums[] = "4433007711";
+
 	while (strn[i] != '\0')
 	{
 		for (j = 0; j < 10; j++)
