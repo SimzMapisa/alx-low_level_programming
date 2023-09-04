ca0440e983e48d73969e9a034df9e05a5248b99b
SimzMapisa <simbarashe795@gmail.com>
2023-09-04 18:22:38 +0200

    fix betty errors

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(sizeof (int) * width);
+		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
 		{
 			for (; i >= 0; i--)
