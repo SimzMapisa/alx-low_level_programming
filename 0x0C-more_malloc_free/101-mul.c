902b9b7586ae8450d3332213622fb10589d13dfb
SimzMapisa <simbarashe795@gmail.com>
2023-09-07 15:42:34 +0200

    Write a program that multiplies two positive numbers.

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * @argc: array length
+ * @argv: array of args
+ *
+ * Description: "multiplies two integers"
+ *
+ * Return: int res
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul, i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; argv[1][i] != '\0'; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (i = 0; argv[2][i] != '\0'; i++)
+	{
+		if (!isdigit(argv[2][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
