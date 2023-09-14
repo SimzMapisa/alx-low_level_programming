e5898eb33daf794ed8779a40afefcf3a668fa841
SimzMapisa <simbarashe795@gmail.com>
2023-09-14 23:38:01 +0200

    fix output issues

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numbers;
 
-	separator = ",";
-
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
 		if (i == n - 1)
+			continue;
+		if (separator == NULL)
 			continue;
 		printf("%s ", separator);
 	}
