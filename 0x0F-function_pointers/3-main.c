25de2a5a2b5403dc7dc6d4471b584f28a6619a32
SimzMapisa <simbarashe795@gmail.com>
2023-09-14 19:10:28 +0200

    fix requirement issue

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,13 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (strcmp(argv[2], "/") == 0 && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	if (strcmp(argv[2], "%") == 0 && atoi(argv[3]) == 0)
+	if (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0 && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
