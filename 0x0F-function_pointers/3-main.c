ee2952228c9a335433e9f13f12112d676a60685a
SimzMapisa <simbarashe795@gmail.com>
2023-09-14 19:16:52 +0200

    fix req error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (!strcmp(argv[2], "/") || !strcmp(argv[2], "%") && atoi(argv[3]) == 0)
+	if ((!strcmp(argv[2], "/") || !strcmp(argv[2], "%")) && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
