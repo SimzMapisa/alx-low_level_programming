6ea9951f3d53ab9e6f06ebf458bf8196e61b4c26
SimzMapisa <simbarashe795@gmail.com>
2023-09-21 19:20:32 +0200

    add logic to calculate the length of data and add the node to the head of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the list
+ * @head: pointer to the head pointer
+ * @str: pointer data
+ *
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+	const char *current_char;
+
+	new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return(NULL);
+	}
+
+	current_char = new_node->str;
+	while(*current_char != '\0')
+	{
+		len++;
+		current_char++;
+	}
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
