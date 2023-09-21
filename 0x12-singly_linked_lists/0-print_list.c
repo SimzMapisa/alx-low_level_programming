cab6a1d6d70b35a9f0e9bd2b1d8c8c9eccf3f688
SimzMapisa <simbarashe795@gmail.com>
2023-09-21 15:23:55 +0200

    print the elements of the lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the lists
+ * @h: pointer to the first element
+ *
+ * Return: "number of elements"
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		printf("List is empty!");
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			count++;
+		}
+	}
+
+	return (count);
+
+}
