a8404aff478b88b44b066e3cf32bf6086fde5356
SimzMapisa <simbarashe795@gmail.com>
2023-11-09 16:01:30 +0200

    create function that adds a node at a specific node

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts node into a doubly linked list.
+ * @h: pointer to head pointer.
+ * @idx: the index.
+ * @n: the value
+ *
+ * Return: new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *copy, *new, *tmp;
+	unsigned int i;
+
+	if (!h || ((*h) == NULL && idx != 0))
+		return (NULL);
+
+	copy = *h;
+	for (i = 0; i < idx; i++)
+	{
+		if (!copy)
+			return (NULL);
+		tmp = copy;
+		copy = copy->next;
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = copy;
+	new->prev = idx == 0 ? NULL : tmp;
+	if (new->prev)
+		tmp->next = new;
+	else
+		*h = new;
+	if (copy)
+		copy->prev = new;
+	return (new);
+}
