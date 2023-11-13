3d835e864d9c128885ffe8edcbc2572c2ea98e49
SimzMapisa <simbarashe795@gmail.com>
2023-11-13 12:35:59 +0200

    create a dynamic library with c function and use them in a python programme

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Quotient of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Remainder of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
